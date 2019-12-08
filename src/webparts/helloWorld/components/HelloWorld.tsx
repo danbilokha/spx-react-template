62b9ce55aab2c268f3db7c14afc68cf202a01b68
Danylo Bilokha <danbilokha@gmail.com>
2019-12-08 18:25:00 +0200

    routing for react

--- src/webparts/helloWorld/components/HelloWorld.tsx
+++ src/webparts/helloWorld/components/HelloWorld.tsx
@@ -1,43 +1,27 @@
 import * as React from 'react';
 import styles from './HelloWorld.module.scss';
-import { IHelloWorldProps } from './IHelloWorldProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+import {IHelloWorldProps} from './IHelloWorldProps';
 import Header from './header/Header';
 import Home from '../containers/home/Home';
-import {Switch, Route} from 'react-router-dom';
 import NotFound from '../containers/notFound/NotFound';
-import { ConnectedRouter } from 'connected-react-router';
 import history from '../services/history';
-import {configureStore, Provider} from 'react-redux';
-
-const store = configureStore({}, history);
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 export default class HelloWorld extends React.Component<IHelloWorldProps, {}> {
   public render(): React.ReactElement<IHelloWorldProps> {
     return (
-      <div className={ styles.helloWorld }>
-        <Header />
-        <Provider store={store}>
-          <ConnectedRouter history={history}>
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="" component={NotFound} />
-            </Switch>        
-          </ConnectedRouter>
-        </Provider>
-        {/* <Home /> */}
-        {/* <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to ITM SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
-            </div>
-          </div>
-        </div> */}
+      <div className={styles.helloWorld}>
+        <Header/>
+        <BrowserRouter history={history}>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="" component={NotFound}/>
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
